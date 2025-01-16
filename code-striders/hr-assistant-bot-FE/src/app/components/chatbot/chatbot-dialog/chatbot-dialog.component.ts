1daeec7d3f4d52fc0ceb3fb7cfe71253e6bb1eb5
Shanmukh0028 <holmes2876@gmail.com>
2025-01-16 11:50:28 +0530

    Basic Functionality and skeleton of ChatBot Popup

--- code-striders/hr-assistant-bot-FE/src/app/components/chatbot/chatbot-dialog/chatbot-dialog.component.ts
+++ code-striders/hr-assistant-bot-FE/src/app/components/chatbot/chatbot-dialog/chatbot-dialog.component.ts
@@ -8,6 +8,11 @@ import { Component } from '@angular/core';
 export class ChatbotDialogComponent {
   isChatOpen = false;
   userMessage = '';
+  chatMessages: {text: string,sender: "customer"|"bot"}[] = [];
+
+  ngOnInit(){
+    this.chatMessages.push({text: "Hi, How Can I help you?",sender:"bot"})
+  }
 
   toggleChat(): void {
     this.isChatOpen = !this.isChatOpen;
@@ -15,8 +20,13 @@ export class ChatbotDialogComponent {
 
   sendMessage(): void {
     if (this.userMessage.trim()) {
+      this.chatMessages.push({text:this.userMessage,sender:"customer"});
       console.log('User message:', this.userMessage);
       this.userMessage = '';
+
+      setTimeout(()=>{
+        this.chatMessages.push({text: "Sure,I am happy to help you",sender:"bot"});
+      },2000)
     }
   }
 }
